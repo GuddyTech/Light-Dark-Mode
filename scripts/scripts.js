e9d63bc61d809d931ca9069195aa2e6bcb225dc0
Okorie GoodGod <guddytechs@gmail.com>
2022-11-18 17:35:48 +0100

    commiting

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+const bodyEl = document.body;
+const buttonEL = document.getElementById("btn");
+// const continue = document.getElementById("cont");
+
+function toggleDarkMode() {
+    return darkMode.toggleAttribute("dark");
+}
+
+
+
+function continuePage() {
+ 
+}
